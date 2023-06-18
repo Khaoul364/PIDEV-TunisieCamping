ff2a9482e374addeace47f7b86453dcfafb65d93
Rymebaazaoui <rym.baazaoui@esprit.tn>
2023-06-18 17:09:14 +0100

    add CrossOrigin annotation to ForumCmntController

--- src/main/java/tn/esprit/spring/controller/ForumCmntController.java
+++ src/main/java/tn/esprit/spring/controller/ForumCmntController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ForumCmntController {
     @Autowired
     IForumCmntService forumCmntService;
